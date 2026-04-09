fd97a905765e84c6415226f4a4ba8d1581b2b1b4
cheecho92 <joshua.j.wisniewski@gmail.com>
2026-04-09 03:01:05 -0400

    idk what changed. I had saved everything

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import secrets
 import string
+import requests
 
 # Generates a randomized state string
 def random_string_generator():
@@ -7,3 +8,12 @@ def random_string_generator():
     state_string = ''.join(secrets.choice(alphabet) for i in range(33))
     return state_string
 
+def authenticated_user(access_token, client_id):
+    headers = {
+        'Authorization': f"Bearer {access_token}",
+        'Client-Id': client_id
+    }
+    req = requests.get("https://api.twitch.tv/helix/users", headers=headers)
+    req.raise_for_status()
+    response = req.json()
+    return response['data'][0]['login']
